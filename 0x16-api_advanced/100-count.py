21417d5f2fa77aada169e3c90a011fd0f1fe2091
ThomaxIgnition <113600602+ThomaxIgnition@users.noreply.github.com>
2023-08-09 04:39:42 +0100

    Update 100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,35 +1,47 @@
 #!/usr/bin/python3
-"""Reddits"""
-import requests
-from collections import Counter
+"""Module for task 3"""
 
 
-def count_words(subreddit, word_list, after=None):
-    """Recurse"""
-    user_agent = "MyRedditBot/1.0"
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    params = {"after": after} if after else {}
-    headers = {"User-Agent": user_agent}
-    response = requests.get(url, headers=headers, params=params)
-    if response.status_code == 200:
-        data = response.json()
-        posts = data["data"]["children"]
-        titles = [post["data"]["title"].lower() for post in posts]
-        keywords = [word.lower() for word in word_list]
-        keyword_counter = Counter()
-        for title in titles:
-            for keyword in keywords:
-                if f' {keyword} ' in f' {title} ':
-                    keyword_counter[keyword] += 1
-        if "after" in data["data"] and data["data"]["after"] is not None:
-            return count_words(subreddit,
-                               word_list, after=data["data"]["after"])
-        else:
-            sorted_keywords = sorted(keyword_counter.items(),
-                                     key=lambda item: (-item[1], item[0]))
-            for keyword, count in sorted_keywords:
-                print(f"{keyword}: {count}")
-    elif response.status_code == 404:
-        print("Invalid subreddit or no results found.")
+def count_words(subreddit, word_list, word_count={}, after=None):
+    """Queries the Reddit API and returns the count of words in
+    word_list in the titles of all the hot posts
+    of the subreddit"""
+    import requests
+
+    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json"
+                            .format(subreddit),
+                            params={"after": after},
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code != 200:
+        return None
+
+    info = sub_info.json()
+
+    hot_l = [child.get("data").get("title")
+             for child in info
+             .get("data")
+             .get("children")]
+    if not hot_l:
+        return None
+
+    word_list = list(dict.fromkeys(word_list))
+
+    if word_count == {}:
+        word_count = {word: 0 for word in word_list}
+
+    for title in hot_l:
+        split_words = title.split(' ')
+        for word in word_list:
+            for s_word in split_words:
+                if s_word.lower() == word.lower():
+                    word_count[word] += 1
+
+    if not info.get("data").get("after"):
+        sorted_counts = sorted(word_count.items(), key=lambda kv: kv[0])
+        sorted_counts = sorted(word_count.items(),
+                               key=lambda kv: kv[1], reverse=True)
+        [print('{}: {}'.format(k, v)) for k, v in sorted_counts if v != 0]
     else:
-        print(f"Error: {response.status_code}")
+        return count_words(subreddit, word_list, word_count,
+                           info.get("data").get("after"))
